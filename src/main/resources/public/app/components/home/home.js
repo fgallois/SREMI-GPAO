7b02e030621be8eed11cd68509ac4747c56738f2
Fabien Gallois <fgallois@gmail.com>
2015-10-17 16:40:36 -0400

    Added validation

--- src/main/resources/public/app/components/home/home.js
+++ src/main/resources/public/app/components/home/home.js
@@ -79,15 +79,20 @@
 
         this.printBL = function () {
             var lineSelection = $('#tableCde').bootstrapTable('getSelections');
-            var dataBL = JSON.stringify({orderRef: bl.orderListLabel, lines:lineSelection});
-            $http.post('./bonLivraison', dataBL)
-                .success(function (data, status, headers, config) {
-                    bl.invoiceNumber = parseInt(headers('receiptNumber'));
-                    window.open(headers('Location'), "_blank");
-                })
-                .error(function (data) {
-                    console.log('Error = ' + data);
-                });
+            if(typeof lineSelection != "undefined" && lineSelection != null && lineSelection.length > 0) {
+                $( "#alert1" ).toggle(false);
+                var dataBL = JSON.stringify({orderRef: bl.orderListLabel, lines: lineSelection});
+                $http.post('./bonLivraison', dataBL)
+                    .success(function (data, status, headers, config) {
+                        bl.invoiceNumber = parseInt(headers('receiptNumber'));
+                        window.open(headers('Location'), "_blank");
+                    })
+                    .error(function (data) {
+                        console.log('Error = ' + data);
+                    });
+            } else {
+                $( "#alert1" ).toggle(true);
+            }
         };
 
         this.updateInvoiceNumber = function () {
